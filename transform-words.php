f245981141bdff93e8b75f23dd64f673b5299b75
Peter Malcolm <peter.t.malcolm@gmail.com>
2015-05-21 13:27:10 -0400

    new boilerplate file with boilerplate code
    new boilerplate file with boilerplate code

---
+++ transform-words.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Transform Words
+Plugin URI: http://petermalcolm.me
+Description: WordPress Plugin for words that transform using jQuery transitions
+Author: Peter Malcolm
+Version: 0.1
+Author URI: http://petermalcolm.me
+*/
+
+defined( 'ABSPATH' ) or die( 'Do not access this file directly.' );
+
+?>
