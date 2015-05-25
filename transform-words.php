f25ec06d58098350e97f4f3f24cf68e5df65b33a
Peter Malcolm <peter.t.malcolm@gmail.com>
2015-05-24 22:25:07 -0400

    Revert "modified license"
    This reverts commit 3e155be2afdf957845724072d1e936c3cdca11d4.
    
    Conflicts:
    	transform-words.php

---
+++ transform-words.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Transform Words
+Plugin URI: http://petermalcolm.me
+Description: WordPress Plugin for words that transform using jQuery transitions
+Author: Peter Malcolm
+Version: 0.1
+Author URI: http://petermalcolm.me
+*/
+
+defined( 'ABSPATH' ) or die( 'Do not access this file directly.' );
+
+function pmtw_install() {
+}
+
+?>
